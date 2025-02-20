542f8a8580a51d6ba7804858621d13b57f8c378e
35989 <35989@DESKTOP-0B5C62I>
2025-02-20 12:34:21 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp4;
+
+class Program
+{
+    static void BubbleSort(Student[] students)
+    {
+        int n = students.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (students[j].Age > students[j + 1].Age)
+                {
+                    // Swap
+                    Student temp = students[j];
+                    students[j] = students[j + 1];
+                    students[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void Main()
+    {
+        Student[] students = {
+            new Student("ivan", 17),
+            new Student("mariya", 16),
+            new Student("petur", 18),
+            new Student("anna", 15)
+        };
+
+        BubbleSort(students);
+
+        foreach (var student in students)
+        {
+            Console.WriteLine($"{student.Name} - {student.Age} godini");
+        }
+    }
+}
+
---
+++ ConsoleApp4/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
